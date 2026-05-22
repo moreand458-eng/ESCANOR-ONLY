52e149fe208a967eb8f770aa1631e1150278dd20
𝑬𝑺𝑪𝑨𝑵𝑶𝑹 <moreand458@gmail.com>
2026-05-22 23:41:33 +0300

    Add files via upload

---
+++ plugins/owner/restart.js
@@ -0,0 +1,20 @@
+const test = async (m, { conn, bot }) => {
+  m.react("🟢")
+  
+  conn.msgUrl(m.chat, "♡゙ Bot is restarting...", { 
+    title: "𝘌𝘚𝘊𝘈𝘕𝘖𝘙 𝘪𝘴 𝘢 𝘞𝘩𝘢𝘵𝘴𝘈𝘱𝘱 𝘣𝘰𝘵 𝘧𝘳𝘰𝘮 𝘵𝘩𝘦 𝘌𝘚𝘊𝘓𝘐𝘕𝘒 𝘓𝘪𝘣𝘳𝘢𝘳𝘺",
+    body: "𝑇𝒉𝑒 𝑏𝑜𝑡 𝑖𝑠 𝑠𝑖𝑚𝑝𝑙𝑒 𝑡𝑜 𝑚𝑜𝑑𝑖𝑓𝑦",
+    img: "https://i.postimg.cc/XJX2cRJc/0af18dd2b2543651464204773234c433.jpg",
+    big: false 
+  });
+  
+  setTimeout(() => {
+    bot.restart();
+  }, 1000); 
+};
+
+test.usage = ["رستارت"]
+test.category = "owner";
+test.command = ["رستارت", "restart"];
+test.owner = true;
+export default test;
